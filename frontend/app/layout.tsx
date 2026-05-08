27aba14a943b86eddf1405b47d1189a157095c24
Junaid Ahmad <junaid@thecozm.com>
2026-05-08 20:37:09 +0500

    added adsesn script

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -34,8 +34,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google AdSense Script Placeholder */}
-        {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX" crossOrigin="anonymous"></script> */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9898224793678981"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
